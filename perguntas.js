f22eb77b6441deb30a526e95d107ca3413604ca0
franciscolinoneto-cmyk <francisco.lino.neto@escola.pr.gov.br>
2026-06-26 17:58:48 -0300

    njlnj

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,35 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Game of Thrones',
+    'Quem foi Daenerys Targaryen?',
+    'Filha do Rei Louco, exilada em Essos. Ficou conhecida como “Mãe dos Dragões” ao chocar três dragões e buscar o Trono de Ferro.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Game of Thrones',
+    'Quem foi Oberyn Martell?',
+    'Príncipe de Dorne conhecido como “Víbora Vermelha”. Buscou vingança pela morte de sua irmã Elia Martell e morreu em julgamento por combate contra Gregor Clegane.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Game of Thrones',
+    'Quem foi Rhaegar Targaryen?',
+    'Príncipe herdeiro Targaryen, filho do Rei Louco. Sua relação com Lyanna Stark desencadeou a Rebelião de Robert, levando à queda dos Targaryen. Também é revelado como pai de Jon Snow'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'House of the Dragon',
+    'Quem foi Rhaenyra Targaryen?',
+    'Filha do rei Viserys I e primeira herdeira mulher do Trono de Ferro. Sua disputa pelo trono gerou a Dança dos Dragões, guerra civil entre Targaryens.'
 )
 
+criaCartao(
+    'House of the Dragon',
+    'Quem foi Jaehaerys I Targaryen?',
+    'Rei Targaryen conhecido como “O Conciliador”, que trouxe estabilidade e paz aos Sete Reinos por meio de leis e diplomacia.'
+)
+
+criaCartao(
+    'House of the Dragon',
+    'Quem foi Vhagar?',
+    'Dragão antigo de Visenya Targaryen, depois montado por Aemond Targaryen na Dança dos Dragões. Um dos maiores e mais poderosos dragões de Westeros.'
+)
